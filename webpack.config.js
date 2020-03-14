459889364a363dc637ddcc198029b219e74f722d
zhangwenyang <zhangwenyang@geyecloud.com>
2020-03-14 11:46:56 +0800

    webpack+typescript

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
